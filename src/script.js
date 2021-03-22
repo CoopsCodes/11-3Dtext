f2f0fb2a2b6fbc812939d9999469fa9eed553f4e
Cooper Viktor <cooper@gracepapers.com.au>
2021-03-22 11:15:34 +1100

    updating with font from Cat, plus adding donuts

--- src/script.js
+++ src/script.js
@@ -1,98 +1,163 @@
-import './style.css'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'dat.gui'
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import * as dat from "dat.gui";
+// can import fonts like this, but will take a long time
+// import typefaceFont from 'three/examples/fonts/helvetiker_regular.typeface.json'
 
 /**
  * Base
  */
 // Debug
-const gui = new dat.GUI()
+const gui = new dat.GUI();
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
+
+// Axis Helper
+const axisHelper = new THREE.AxesHelper();
+// scene.add(axisHelper);
 
 /**
  * Textures
  */
-const textureLoader = new THREE.TextureLoader()
+const textureLoader = new THREE.TextureLoader();
+const madcapTexture = textureLoader.load("/textures/matcaps/7.png");
+
+console.log(madcapTexture);
+
+/**
+ * Font Loader
+ */
+const fontLoader = new THREE.FontLoader();
+fontLoader.load("/font/Grmlincore_Regular.json", (font) => {
+  const textGeometry = new THREE.TextBufferGeometry("Fuck the patriarchy!", {
+    font: font,
+    size: 0.5,
+    height: 0.2,
+    curveSegments: 5,
+    bevelEnabled: true,
+    bevelThickness: 0.03,
+    bevelSize: 0.02,
+    bevelOffset: 0,
+    bevelSegments: 4,
+  });
+  // this is the manual way to //-> textGeometry.center();
+  //   textGeometry.computeBoundingBox();
+  //   textGeometry.translate(
+  //     -textGeometry.boundingBox.max.x * 0.5,
+  //     -textGeometry.boundingBox.max.y * 0.5,
+  //     -textGeometry.boundingBox.max.z * 0.5
+  //   );
+
+  //-> places the axis at the center of the text
+  textGeometry.center();
+
+  const material = new THREE.MeshNormalMaterial();
+  //   textMaterial.matcap = madcapTexture;
+  const text = new THREE.Mesh(textGeometry, material);
+  scene.add(text);
+
+  const donutGeometry = new THREE.TorusBufferGeometry(0.3, 0.2, 20, 45);
+
+  for (let i = 0; i < 100; i++) {
+    const donut = new THREE.Mesh(donutGeometry, material);
+    donut.position.x = (Math.random() - 0.5) * 10;
+    donut.position.y = (Math.random() - 0.5) * 10;
+    donut.position.z = (Math.random() - 0.5) * 10;
+
+    donut.rotation.x = Math.random() * Math.PI;
+    donut.rotation.y = Math.random() * Math.PI;
+
+    const scale = Math.random();
+
+    donut.scale.set(scale, scale, scale);
+    scene.add(donut);
+  }
+});
 
 /**
  * Object
  */
 const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial()
+);
 
-scene.add(cube)
+// scene.add(cube);
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 1
-camera.position.y = 1
-camera.position.z = 2
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.x = 1;
+camera.position.y = 1;
+camera.position.z = 2;
+scene.add(camera);
 
 // Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
  * Animate
  */
-const clock = new THREE.Clock()
+const clock = new THREE.Clock();
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
 
-    // Update controls
-    controls.update()
+  // Update controls
+  controls.update();
 
-    // Render
-    renderer.render(scene, camera)
+  //   donut.rotation.x += 0.01;
+  //   donut.rotation.y += 0.02;
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
+  // Render
+  renderer.render(scene, camera);
 
-tick()
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
